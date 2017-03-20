20961fd207f73d2e463b663a81d31967254afb8e
标豆 <biaobean@gmail.com>
2017-03-20 11:59:43 +0800

    Create build.sh

---
+++ bin/build.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+cd $BRANCH_BF_ROOT_DIR/parquet-format
+mvn clean install -DskipTests
+
+cd $BRANCH_BF_ROOT_DIR/parquet-mr
+mvn clean install -DskipTests
+
+cd $BRANCH_BF_ROOT_DIR/spark
+rm spark-2.1.0-SNAPSHOT-bin-custom-spark.tgz
+./build/mvn -Pyarn -Phadoop-2.6 -Dhadoop.version=2.6.0 -DskipTests clean package
+
+./dev/make-distribution.sh --name custom-spark --tgz -Phadoop-2.6 -Pyarn
+
+cd $BRANCH_BF_ROOT_DIR/impala
+export IMPALA_HOME=`pwd`
+source $IMPALA_HOME/bin/impala-config.sh
+./buildall.sh -notests -build_shared_libs
