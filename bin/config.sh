27bbe62029c60f7530386680c4ee636916fec7e2
标豆 <biaobean@gmail.com>
2017-03-21 13:26:02 +0800

    Update config.sh

--- bin/config.sh
+++ bin/config.sh
@@ -1,2 +1,6 @@
-rm -rf ../conf
-cp /opt/cloudera/parcels/CDH/lib/hive/conf/ ..
+rm -rf $BRANCH_BF_ROOT_DIR/conf
+mkdir $BRANCH_BF_ROOT_DIR/conf
+cp /opt/cloudera/parcels/CDH/lib/hive/conf/* $BRANCH_BF_ROOT_DIR/conf
+
+rm -rf $BRANCH_BF_ROOT_DIR/incubator-impala/fe/src/test/resources
+ln -s $BRANCH_BF_ROOT_DIR/conf $BRANCH_BF_ROOT_DIR/incubator-impala/fe/src/test/resources
